9135c3abe947102036e81d1fcccb384ed19bef98
mostafaIn <mostafa2015se@gmail.com>
2020-10-25 13:57:30 +0200

    9.9: Patientor backend, step2

--- patientor_backend/build/index.js
+++ patientor_backend/build/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
 var app = express_1.default();
 app.use(express_1.default.json());
-var PORT = 3000;
+app.use(cors_1.default());
+var PORT = 3001;
 app.get('/ping', function (_req, res) {
     res.send('pong');
 });
--- patientor_backend/index.ts
+++ patientor_backend/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
